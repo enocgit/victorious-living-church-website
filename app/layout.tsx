a82ed898c4b402da227cfbc13766c666f9dca625
Enoch Ansah <enocansah@yahoo.com>
2025-06-26 12:40:02 +0000

    Set up theme, typography and add shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Kumbh_Sans, Nunito } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const kumbhSans = Kumbh_Sans({
+  variable: "--font-kumbh-sans",
+  subsets: ["latin"],
+});
+
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} ${kumbhSans.variable} ${nunito.variable} antialiased`}
       >
         {children}
       </body>
---
+++ components/ui/typography.tsx
@@ -0,0 +1,68 @@
+import { FC, HTMLProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+type ElementTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+type TypographyVariant =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "body1"
+  | "body2"
+  | "body3";
+
+type TypographyProps = HTMLProps<HTMLParagraphElement> & {
+  variant?: TypographyVariant;
+  component?: ElementTypes;
+} & (
+    | { text: string; children?: never }
+    | { children: ReactNode; text?: never }
+  );
+
+const variantClasses: Record<TypographyVariant, string> = {
+  h1: "text-6xl font-bold tracking-tighter lg:text-8xl",
+  h2: "text-4xl font-bold tracking-tight lg:text-5xl",
+  h3: "text-3xl font-bold tracking-tight lg:text-4xl",
+  h4: "font-bold text-3xl",
+  h5: "font-bold text-lg",
+  h6: "font-bold text-sm",
+  body1: "text-base",
+  body2: "text-sm",
+  body3: "text-xs",
+};
+
+export const Typography: FC<TypographyProps> = ({
+  variant = "body1",
+  component,
+  children,
+  text,
+  className,
+  ...rest
+}) => {
+  if ((text && children) || (!text && !children)) {
+    throw new Error(
+      "[Typography]: You must pass either 'text' or 'children', and not both"
+    );
+  }
+
+  const isHeading =
+    variant === "h1" ||
+    variant === "h2" ||
+    variant === "h3" ||
+    variant === "h4" ||
+    variant === "h5" ||
+    variant === "h6";
+
+  const classes = variantClasses[variant] || "";
+  const finalClasses = twMerge(classes, className);
+
+  const ValidComponent = component || (isHeading ? variant : "p");
+
+  return (
+    <ValidComponent className={finalClasses} {...rest}>
+      {text ?? children}
+    </ValidComponent>
+  );
+};
