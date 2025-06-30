616ddcaca052aea6e4ff4226dc5cb4dd4df7c99d
Enoch Ansah <enocansah@yahoo.com>
2025-06-30 12:27:30 +0000

    Add apple-mobile-web-app-title meta tag to layout and update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Vlcig" />
+      </head>
       <body
         className={`${geistMono.variable} ${kumbhSans.variable} ${nunito.variable} flex min-h-screen flex-col antialiased`}
       >
